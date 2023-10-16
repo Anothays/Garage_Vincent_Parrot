8e2ed8ad32300bfdd5d66116006affc2ae16d1c9
Jeremy Sananikone <jeremy.snnk@gmail.com>
2023-10-16 22:03:26 +0200

    create js

---
+++ public/js/contactPage.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+    initPage()
+})
+
+function initPage() {
+
+    // notification toast
+    const notification = new NotificationToast('liveToast')
+    // const toastLiveExample = document.getElementById('liveToast')
+    // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+
+    // handle formSubmit
+    const form = document.getElementById('contactForm')
+    form.addEventListener('submit', function(e) {
+        e.preventDefault()
+        console.log(e.target)
+        fetch(this.action, {
+            body: new FormData(e.target),
+            method: 'POST',
+            headers: {"X-Requested-With": "XMLHttpRequest"}
+        })
+            .then(res => res.json())
+            .then(res => {
+                notification.setMessage(res.message)
+                notification.show()
+                e.target.reset()
+            })
+            .catch(error => console.log(error))
+    })
+}
